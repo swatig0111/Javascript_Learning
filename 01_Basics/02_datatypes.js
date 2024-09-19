ed2588002c026de88de6bfb678bbeafe7f32d4b4
Swati Gamit <92017096+swatig0111@users.noreply.github.com>
2024-09-19 09:05:05 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"  //treat al Js code as newer version
+
+
+console.log("Datatypes")
+
+let name = "swati"
+let age = 22
+let isLoggedin = false 
+
+
+//number -> 2 to the power 53 
+//big int -> used for large dataset 
+//string -> write it in " " or ' ' quotes 
+//boolean -> gives true or false value 
+//null -> standalone value, representation of empty value 
+//undefined 
+//symbol -> unique
+
+
+//object 
+
+console.log(typeof "swati");
+console.log(typeof age);
+
+
+
+
+
